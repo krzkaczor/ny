dab0cb61b469c4e5f608f1849a42cf5ce1e3c088
Krzysztof Kaczor <krzkaczor@gmail.com>
2025-06-29 16:03:19 +0200

    Optimize nested run calls

--- src/common/commands/run.rs
+++ src/common/commands/run.rs
@@ -3,6 +3,7 @@ use eyre::{ContextCompat, Result};
 use std::path::Path;
 
 use crate::{agent::Agent, execute::Executor, fs::find_in_parents, fs::Filesystem};
+use crate::commands::run_optimized::try_optimize_script_execution;
 
 pub fn run(
     executor: &dyn Executor,
@@ -32,10 +33,24 @@ pub fn run(
 
     let bin_path = construct_path_env(package_json_path.parent().unwrap());
 
+    run_script_or_program(executor, &agent, task, extra_args, &package_json, bin_path)
+}
+
+fn run_script_or_program(executor: &dyn Executor, agent: &Agent, task: &str, extra_args: Option<&[&str]>, package_json: &serde_json::Value, bin_path: String) -> Result<()> {
     if let Some(mut script) = load_script(&package_json, task) {
         if let Some(extra_args) = extra_args {
             script += " ";
             script += &extra_args.join(" ")
+        }
+
+        // try to optimize execution if script is only combining multiple other commands like "pnpm run lint && pnpm run test"
+        let subscripts = try_optimize_script_execution(agent, &script);
+
+        if let Some(subscripts) = subscripts {
+            for subscript in subscripts {
+                run_script_or_program(executor, agent, &subscript, None, &package_json, bin_path.clone())?
+            }
+            return Ok(());
         }
 
         println!("{}", format!("$ {script}").dimmed());
---
+++ src/common/commands/run_optimized.rs
@@ -0,0 +1,46 @@
+use crate::agent::Agent;
+
+pub fn try_optimize_script_execution<'a>(agent: &'a Agent, script: &'a str) -> Option<Vec<&'a str>> {
+    let run_prefix = agent_to_run_command(agent);
+    let chunks: Vec<_> = script.split("&&").map(str::trim).collect();
+
+    let optimizable = chunks.iter().all(|chunk| {
+        chunk.starts_with(run_prefix)
+    });
+
+    if !optimizable {
+        return None;
+    }
+
+    let stripped = chunks.iter().map(|chunk| chunk[run_prefix.len()..].trim());
+
+    Some(stripped.collect())
+}
+
+// @todo: this could return also common shortcuts like npm test
+fn agent_to_run_command(agent: &Agent) -> &str {
+    match agent {
+        Agent::Npm => "npm run ",
+        Agent::Pnpm => "pnpm run ",
+        Agent::Yarn => "yarn run ",
+        Agent::Bun => "bun run ",
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_stripping_pnpm_runs() {
+        let result = try_optimize_script_execution(&Agent::Pnpm, "pnpm run a && pnpm run b");
+        assert!(result.is_some());
+        assert_eq!(result.unwrap(), vec!["a", "b"]);
+    }
+
+    #[test]
+    fn test_does_not_optimize_other_calls() {
+        let result = try_optimize_script_execution(&Agent::Pnpm, "mocha --timeout=0");
+        assert!(result.is_none());
+    }
+}
