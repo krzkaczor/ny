bd37eb2f668f10c6c8cfd3d94d2af6bd6d23fa54
Krzysztof Kaczor <krzkaczor@gmail.com>
2025-07-19 12:56:22 +0200

    Remove old e2e tests

--- test-e2e/test/setup.ts
+++
@@ -1,6 +0,0 @@
-import { beforeAll } from "bun:test";
-import { executeNy } from "./test-utils";
-
-beforeAll(async () => {
-  await executeNy("", { path: "../sandboxes/yarn" }); // install deps
-});
